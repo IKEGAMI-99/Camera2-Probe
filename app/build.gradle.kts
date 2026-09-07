a8f1fc8eb27b51082a24445028783e5ac467e26e
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-07 09:02:55 +0900

    Bump to v0.1.1 and remove AndroidX startup dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ikegami.camera2probe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
@@ -35,6 +35,4 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
 }
