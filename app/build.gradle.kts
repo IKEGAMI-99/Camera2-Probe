95281780075f9e4441c3d6ec23ac1fcc97573f0f
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-07 09:18:52 +0900

    Bump Camera2 Probe to v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ikegami.camera2probe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
     }
 
     buildTypes {
