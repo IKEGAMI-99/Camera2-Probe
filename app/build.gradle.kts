06a8cd3d17cabecf1e705eedb951dfa3725cc205
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-07 13:22:10 +0900

    Bump TRI CAM to v0.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ikegami.camera2probe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.5.2"
+        versionCode = 11
+        versionName = "0.5.3"
     }
 
     buildTypes {
